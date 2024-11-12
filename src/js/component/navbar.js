f0f6db992e76de9ce66e5a996e48b82008992df0
Jordan Campos Garro <114543285+jcamposg6991@users.noreply.github.com>
2024-11-12 00:28:19 +0000

    codificacion frontend navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,12 +5,18 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<img className="ms-5" src="https://www.star-wars.se/wp-content/uploads/2023/01/toppng.com-star-wars-logos-icons-vector-star-wars-felirat-keszites-2059x881-1.png" alt="" width="120" height="60" />
 			</Link>
 			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+				<div class="btn-group me-5" role="group">
+					<button id="btnGroupDrop1" type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+						Dropdown
+					</button>
+					<ul class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+						<li><a class="dropdown-item" href="#">Dropdown link</a></li>
+						<li><a class="dropdown-item" href="#">Dropdown link</a></li>
+					</ul>
+				</div>
 			</div>
 		</nav>
 	);
